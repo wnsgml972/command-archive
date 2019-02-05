a32ad4c20d6e37c983da32c267490a5a3aeb60a0
JunHee Kim <wnsgml972@gmail.com>
2019-02-05 20:55:26 +0900

    Update set-tomcat8.sh

--- sh/set-tomcat8.sh
+++ sh/set-tomcat8.sh
@@ -2,9 +2,9 @@ sudo apt-get update
 sudo apt-get install openjdk-8-jre-headless
 sudo apt-get install openjdk-8-jdk
 cd /home/user/
-wget http://apache.tt.co.kr/tomcat/tomcat-8/v8.5.33/bin/apache-tomcat-8.5.33.tar.gz
-tar -zxvf apache-tomcat-8.5.33.tar.gz
-mv apache-tomcat-8.5.33 tomcat8
+wget http://apache.tt.co.kr/tomcat/tomcat-8/v8.5.37/bin/apache-tomcat-8.5.37.tar.gz
+tar -zxvf apache-tomcat-8.5.37.tar.gz
+mv apache-tomcat-8.5.37 tomcat8
 cd tomcat8/bin
 sudo ./startup.sh
-sudo rm /home/user/apache-tomcat-8.5.33.tar.gz
+sudo rm /home/user/apache-tomcat-8.5.37.tar.gz
